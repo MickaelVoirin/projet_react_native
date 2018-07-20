9a89a6a14128c17b7e1438e37cb1c0a9711aa844
MickaelVoirin <crjpwork@gmail.com>
2018-07-20 11:08:14 +0200

    Mickael - Navigation de base pour débuter

---
+++ HappyLedger/Navigation/Menu.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StyleSheet, Button, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+export default class MyMenu extends React.Component {
+    
+  constructor(props){
+    super(props);     
+    this.state = {
+      buttons : [
+          {title : 'Connexion', color : '#841584', key : 'Connection'},
+          {title : 'Inscription', color : 'gray', key : 'Registration'},
+          {title : 'Compte', color : 'gray', key : 'Account'},
+          {title : 'Profil', color : 'gray', key : 'Profile'},
+      ]
+    };
+  }
+
+  componentDidMount(){
+      const buttons = [...this.state.buttons];  
+      buttons.map(x => {
+        x.color = (x.key === Actions.currentScene) ? '#841584': 'gray';
+      });
+      this.setState({buttons});
+  }
+
+  render () {
+    const buttons = [...this.state.buttons];
+    return (
+        <View style={styles.container}>
+          <View style={styles.navBar}>
+          { buttons.map((x,i) => 
+            <Button
+                key={i}
+                onPress={() => Actions[x.key].call()} 
+                title={x.title}
+                color={x.color}
+          />
+          )}
+          </View>
+        </View>
+    )
+  }
+  
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10 
+  },
+  navBar: {
+    flexDirection: 'row',
+  },
+});
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # bordeaux-0618-happyledger
+
+
+## Installed Modules
+ 
+#react-native-router-flux
+Description : to switch between views
+Install command : npm i react-native-router-flux --save
+Documentation : 
+https://www.npmjs.com/package/react-native-router-flux
+https://github.com/aksonov/react-native-router-flux/blob/HEAD/docs/API.md
+https://www.youtube.com/watch?v=JKIMbSXlMkY
+
+
