3d6821474ca4f91e79a17fff36b3c0a7c33aa936
Humch <fabienschlegel@yahoo.fr>
2018-08-26 15:08:55 +0200

    modification  de la route de login : vérification à partir du fichier json envoi du token en retour

--- back/routes/user/user.js
+++ back/routes/user/user.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import fs from 'fs';
 
 const router = express.Router();
 
+// Vérification du login de l'utilisateur
+
 router.post('/login', (req, res) => {
-  res.send('LOGIN TOI');
+  const loginData = JSON.parse(fs.readFileSync('./json_files/requetes/user/user_login.json', 'UTF-8'));
+  if (loginData.email !== req.body.email || loginData.password !== req.body.password) {
+    res.send({ message: 'login failed' });
+  } else {
+    const successLogin = fs.readFileSync('./json_test/user/user_login_a.json', 'UTF-8');
+    res.send(successLogin);
+  }
 });
 
 router.post('/register', (req, res) => {
